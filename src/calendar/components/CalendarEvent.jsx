ffa427db0071dabdc65fdf11f6573a884c36f7e7
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-10-03 20:04:54 -0600

    creating modal

--- src/calendar/components/CalendarEvent.jsx
+++ src/calendar/components/CalendarEvent.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 export const CalendarEvent = ({ event }) => {
-    const { title, user } = event;
+  const { title, user } = event;
+
   return (
     <>
-        <strong>{ title }</strong>
-        <span> - { user.name }</span>
-    </> 
+      <strong>{title}</strong>
+      <span> - {user.name}</span>
+    </>
   );
 };
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { addHours } from 'date-fns';
-import { CalendarEvent, Navbar } from '../';
+import { CalendarEvent, CalendarModal, Navbar } from '../';
 import { localizer, getMessagesES } from '../../helpers';
 
 const events = [{
@@ -67,6 +67,8 @@ export const CalendarPage = () => {
         onSelectEvent={ onSelect }
         onView={ onViewChanged }
       />
+
+      <CalendarModal />
 
     </>
   );
