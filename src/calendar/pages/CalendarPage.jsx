63ccd2ff3aebc72aa757b5e577c0bc37fce891be
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-10-03 19:47:36 -0600

    some calendar events, save lastView in localStorage

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { addHours } from 'date-fns';
@@ -18,6 +18,7 @@ const events = [{
 }];
 
 export const CalendarPage = () => {
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
 
   const eventStyleGetter = ( event, start, end, isSelected ) => {
     const style = {
@@ -32,6 +33,19 @@ export const CalendarPage = () => {
     };
   };
 
+  const onDoubleClick = ( event ) => {
+    console.log({ doubleClick: event });
+  };
+
+  const onSelect = ( event ) => {
+    console.log({ click: event });
+  };
+
+  const onViewChanged = ( event ) => {
+    localStorage.setItem('lastView', event);
+    setLastView( event );
+  };
+
   return (
     <>
       <Navbar />
@@ -40,6 +54,7 @@ export const CalendarPage = () => {
         culture='es'
         localizer={localizer}
         events={events}
+        defaultView={ lastView }
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 80px)' }}
@@ -48,6 +63,9 @@ export const CalendarPage = () => {
         components={{
           event: CalendarEvent
         }}
+        onDoubleClickEvent={ onDoubleClick }
+        onSelectEvent={ onSelect }
+        onView={ onViewChanged }
       />
 
     </>
